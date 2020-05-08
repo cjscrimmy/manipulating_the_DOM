30bc8107e5c6bf71bfa58fb344a02ff938ed2f90
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-08 17:36:19 +0100

    add helper method to add a new game's data to the list. use helper method in the event handler to display data on the screen

--- js/app.js
+++ js/app.js
@@ -5,5 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleNewItemFormSubmit = function (event) {
     event.preventDefault();
-    console.log("test");
+
+    const gameListItem = createGameListItem(event.target);
+    const gameList = document.querySelector("#game-list");
+    gameList.appendChild(gameListItem);
+
+    event.target.reset();
+}
+
+const createGameListItem = function (form) {
+    const gameListItem = document.createElement('li');
+    gameListItem.classList.add('game-list-item');
+
+    const title = document.createElement('h3');
+    title.textContent = form.title.value;
+    gameListItem.appendChild(title);
+
+    const genre = document.createElement('p');
+    genre.textContent = `Genre: ${form.genre.value}`;
+    gameListItem.appendChild(genre);
+
+    const platform = document.createElement('p');
+    platform.textContent = `Platform: ${form.platform.value}`;
+    gameListItem.appendChild(platform);
+
+    const numPlayers = document.createElement('p');
+    numPlayers.textContent = `Number of Players: ${form.numPlayers.value}`;
+    gameListItem.appendChild(numPlayers);
+
+    return gameListItem;
 }
